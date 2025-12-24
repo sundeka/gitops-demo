388873714d900d1e771495251f621e773bf2c7aa
Kalle Sundell <sundellkalle@gmail.com>
2025-12-24 14:26:34 +0200

    restructure terraform directory

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,4 +4,14 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
 }
